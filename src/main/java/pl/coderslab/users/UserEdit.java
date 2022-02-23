80d6791bc3bc9bc64e9a0479b53f5abd6879d012
Krzysztof Ficek <krzysztof.ficek88@gmail.com>
2022-02-23 19:02:06 +0100

    1 commit

---
+++ src/main/java/pl/coderslab/users/UserEdit.java
@@ -0,0 +1,17 @@
+package pl.coderslab.users;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet("/user/edit")
+public class UserEdit extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+    }
+}
---
+++ src/main/java/pl/coderslab/users/UserList.java
@@ -0,0 +1,22 @@
+package pl.coderslab.users;
+
+import pl.coderslab.workshop3.UserDao;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet("/user/list")
+public class UserList extends HttpServlet {
+
+
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        UserDao userDao = new UserDao();
+        req.setAttribute("users", userDao.findAllUsers());
+    }
+}
